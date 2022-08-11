45604be8b2d6c591718d01ec1ece025f926f2cdd
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-11 22:48:39 +0000

    Cria arquivo connection.js e escreve casos de sucesso no productModel.js

---
+++ models/productModel.js
@@ -0,0 +1,17 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [result] = await connection.execute(
+    'SELECT id, name FROM StoreManager.products ORDER BY id'
+  );
+
+  return result;
+}
+
+const getById = async (id) => {
+  const [result] = await connection.execute(
+    'SELECT id, name FROM StoreManager.products WHERE id = ?', [id]
+  );
+
+  return result;
+}
