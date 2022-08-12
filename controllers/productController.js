928c11de35cf51a60c1523894f66d6e643cb40b6
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-12 18:42:07 +0000

    lint

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,7 +14,7 @@ const getById = async (request, response) => {
   const { id } = request.params;
   const product = await ProductService.getById(id);
   if (!product) {
-    return response.status(404).json({ message: "Product not found" });
+    return response.status(404).json({ message: 'Product not found' });
   }
 
   return response.status(200).json(product);
