a3032fbc1686b90e81cc23dfe2665661c15516c4
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-12 19:26:05 +0000

    add: arquivos de testes vazios das camadas model, service e controller e um mock da tabela products

---
+++ tests/unit/models/mockProductsTable.js
@@ -0,0 +1,16 @@
+const mockProductsTable = [
+  {
+    id: 1,
+    name: "Martelo de Thor"
+  },
+  {
+    id: 2,
+    name: "Traje de encolhimento"
+  },
+  {
+    id: 3,
+    name: "Escudo do Capitão América"
+  }
+];
+
+module.exports = mockProductsTable;
