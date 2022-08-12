c15640b24d64dcb29abc7588605ea59a65dfc078
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-12 21:24:56 +0000

    Testes da camada productServer: add -> testa se retorno da funcao getAll e um objeto

--- tests/unit/services/productService.test.js
+++ tests/unit/services/productService.test.js
@@ -3,14 +3,26 @@ const { expect } = require('chai');
 
 const ProductModel = require('../../../models/productModel');
 const ProductService = require('../../../services/productService');
+const mockProducts = require('./mockProductsTable');
 
+const allProducts = mockProducts.mockAll[0][0];
+const oneProduct = mockProducts.mockOne[0][0];
 
 describe('Camada service recebe dados com possíveis validações', () => {
-  before(async () => {
-    
-  })
+  
   describe('da listagem de todos os produtos', () => {
-
+    before(async () => {
+      sinon.stub(ProductModel, 'getAll').resolves(allProducts);
+    });
+    after(async () => {
+      ProductModel.getAll.restore();
+    });
+    describe('quando a lista de produtos é carregada com sucesso', () => {
+      it('Seu retorno é um objeto', () => {
+        const products = ProductService.getAll();
+        expect(products).to.be.an('object');
+      })
+    }) 
   });
   describe('do produto por id', () => {
 
