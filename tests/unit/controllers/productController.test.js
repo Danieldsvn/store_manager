e54573414fbbd41aa14d3685aabe4a55f5dd2362
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-13 21:05:54 +0000

    add: Testes da funcao getAll em productController.test.js

--- tests/unit/controllers/productController.test.js
+++ tests/unit/controllers/productController.test.js
@@ -10,10 +10,41 @@ const oneProduct = mockProducts.mockOne[0];
 
 describe('Teste Product Controller', () => {
   describe('Ao receber os produtos', () => {
-    describe('quando for a lista completa', () => {
-      it('se falhar', () => {
-        
+    describe('quando a lista completa não for retornada', () => {
+      before( async () => { 
+        sinon.stub(ProductService, 'getAll').resolves(undefined);        
+      });
+      after(async () => {
+        ProductService.getAll.restore();
+      });
+      it('O status da response seja 400', async () => {
+        const request = {};
+        const response = {};
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        await ProductController.getAll(request, response);        
+        expect(response.status.calledWith(400)).to.be.true;
       });
     });    
+    describe('quando a lista completa for retornada', () => {
+      before(async () => {
+        sinon.stub(ProductService, 'getAll').resolves(allProducts);              
+      });
+      after(async () => {
+        ProductService.getAll.restore();
+      });
+      it('O status da response seja 200', async () => {
+        const request = {};
+        const response = {};  
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        await ProductController.getAll(request, response);
+        expect(response.status.calledWith(200)).to.be.true;
+      })
+    });
   });
 });
