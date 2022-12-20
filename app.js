a4bb9286e217b6eb508002c804c06741c8b956ad
Daniel Saraiva <danieldsvn@gmail.com>
2022-12-20 16:03:37 -0300

    Acrescenta ponto em comentário

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const SaleController = require('./controllers/saleController');
 const app = express();
 app.use(express.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
+// não remova esse endpoint, é para o avaliador funcionar.
 app.get('/', (_request, response) => {
   response.send();
 });
